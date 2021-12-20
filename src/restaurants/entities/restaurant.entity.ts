78f86b71a72088f13c4bd5610717f986c3d0f1d9
Agi-Developer <agilandeena41@gmail.com>
2021-12-20 22:35:46 +0800

    add database with graphql

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,13 +1,32 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+// 1. graphql takes build automaticall genereted our schema
+// 2. entity data orm communicate to database
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field((type) => Number)
+  id: number;
+
   @Field((type) => String)
+  @Column()
   name: string;
+
   @Field((type) => Boolean)
+  @Column()
   isVegan?: boolean;
+
   @Field((type) => String)
+  @Column()
   address: string;
+
   @Field((type) => String)
+  @Column()
   ownersName: string;
+
+  @Field((type) => String)
+  @Column()
+  categoryName: string;
 }
